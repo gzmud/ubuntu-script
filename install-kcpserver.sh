d9c9ddbfd0dfba34e8013ec90b4a8c5b8016ed1e
gzmud <gzimud@gmail.com>
2016-08-20 01:30:01 +0800

    add install-kcpserver.sh

---
+++ install-kcpserver.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+mkdir $HOME/kcptun
+cd $HOME/kcptun
+wget --no-check-certificate https://github.com/xtaci/kcptun/releases/download/v20160819/kcptun-linux-amd64-20160819.tar.gz -O ./kcptun-linux-amd64.tar.g
+
+tar 每zxf kcptun每linux每amd64每*.tar.gz
